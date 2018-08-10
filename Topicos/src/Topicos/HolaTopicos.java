813105e66ab7201399b95fc72512593b239d74cd
Alexis <alexislagarde@hotmail.com>
2018-08-10 19:14:59 -0300

    Primer commit
    Implementacion de un nuevo mensaje de bienvenida

---
+++ Topicos/src/Topicos/HolaTopicos.java
@@ -0,0 +1,10 @@
+package Topicos;
+
+public class HolaTopicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hola, esto es Topicos");
+	}
+
+}
