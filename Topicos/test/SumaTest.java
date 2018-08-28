fc5a4cc3df8599dbd328558828d6b413679dd0ad
Alexis Lagarde <alexislagarde@hotmail.com>
2018-08-28 16:11:28 -0300

    test #3: no pasa

--- Topicos/test/SumaTest.java
+++ Topicos/test/SumaTest.java
@@ -15,9 +15,18 @@ public class SumaTest {
 	@Test
 	public void SumaEnteroNegativo() {
 		Calculadora2 calcu = new Calculadora2();
-		 int esperado= 4;
+		 int esperado= -4;
 		assertEquals (esperado, calcu.suma(-2,-2));
     	}
+	
+	@Test
+	public void SumaNoEntero(){
+		Calculadora2 calcu = new Calculadora2();
+		double esperado = 4.00;
+		assertEquals (esperado, calcu.suma(2.00, 2.00));		
+		
+	}
+	
 
 }
 	
