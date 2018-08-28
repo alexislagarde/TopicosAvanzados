de3ed19c8e2e65aa1218c7828c63bcf1eda686e7
Alexis Lagarde <alexislagarde@hotmail.com>
2018-08-28 16:25:16 -0300

    test double pasa

--- Topicos/test/SumaTest.java
+++ Topicos/test/SumaTest.java
@@ -8,7 +8,7 @@ public class SumaTest {
 	public void SumaEntero() {
 		Calculadora2 calcu = new Calculadora2();
 		 int esperado= 4;
-		assertEquals (esperado, calcu.suma(2,2));
+		assertEquals (esperado, calcu.suma(2,2),0);
 	
 	
 	}
@@ -16,14 +16,14 @@ public class SumaTest {
 	public void SumaEnteroNegativo() {
 		Calculadora2 calcu = new Calculadora2();
 		 int esperado= -4;
-		assertEquals (esperado, calcu.suma(-2,-2));
+		assertEquals (esperado, calcu.suma(-2,-2),0);
     	}
 	
 	@Test
 	public void SumaNoEntero(){
 		Calculadora2 calcu = new Calculadora2();
 		double esperado = 4.00;
-		assertEquals (esperado, calcu.suma(2.00, 2.00));		
+		assertEquals (esperado, calcu.suma(2.00, 2.00),0);		
 		
 	}
 	
